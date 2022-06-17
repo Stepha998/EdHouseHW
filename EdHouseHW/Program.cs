ac981c39421a5db495f03295e14068850d162e75
Štefan Pekník <xpekni01@vutbr.cz>
2022-06-17 20:56:48 +0200

    Add project files.

---
+++ EdHouseHW/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EdHouseHW
+{
+    internal class Program
+    {
+        private const string File = "input.txt";
+
+        public static List<Driver> Drivers = new();
+
+        static void Main(string[] args)
+        {
+            // choose where to find input
+            if (System.IO.File.Exists(File))
+            {
+                FileInput();
+            }
+            else if (args.Length > 0)
+            {
+                Console.WriteLine("arg");
+            }
+            else
+            {
+                Console.WriteLine("keyboard");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void FileInput()
+        {
+            try
+            {
+                // open file with input
+                using (StreamReader sr = new StreamReader(File, Encoding.UTF8))
+                {
+                    // get lunch interval
+                    string[] firstLine = sr.ReadLine()?.Split('-');
+                    int[] lunchInterval = new int[2];
+                    if (firstLine == null || firstLine.Length != 2 || !int.TryParse(firstLine[0], out lunchInterval[0]) || !int.TryParse(firstLine[1], out lunchInterval[1]))
+                    {
+                        ErrorMsg("lunch brake interval specified incorrectly");
+                    }
+
+                    // get trackList info of the individual drivers
+                    while (!sr.EndOfStream)
+                    {
+                        // get trackList and validate it
+                        string[] directions = sr.ReadLine()?.Split(',');
+
+                        Drivers.Add(new Driver(new Track(directions), lunchInterval));
+
+                    }
+
+                    foreach (var driv in Drivers)
+                    {
+                        driv.PrintDriver();
+                    }
+
+                }
+            }
+            catch (FileLoadException)
+            {
+                ErrorMsg("file could not be loaded");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                ErrorMsg("file could not be loaded");
+            }
+        }
+
+        public static void ErrorMsg(string err)
+        {
+            throw new Exception($"ERROR: {err}");
+        }
+
+        
+    }
+}
---
+++ EdHouseHW/Track.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+
+namespace EdHouseHW
+{
+    internal class Track
+    {
+        public List<Point> TrackList { get; }
+
+        public Track(string[] directions)
+        {
+            TrackList = new List<Point>(){new Point(0,0)};
+            CreateTrack(directions);
+        }
+
+        public Track(Track track, int firstIndex, int lastIndex)
+        {
+            if (firstIndex > track.TrackList.Count)
+            {
+                Program.ErrorMsg("driver finishes work before lunch break");
+            }
+            this.TrackList = track.TrackList.GetRange(firstIndex, (lastIndex - firstIndex + 1 > track.TrackList.Count ? track.TrackList.Count : lastIndex - firstIndex + 1));
+        }
+
+        private void CreateTrack(string[] directions)
+        {
+            for (int i = 0; i < directions.Length; i++)
+            {
+                ValidateDirection(directions[i]);
+                CreateDirection(directions[i]);
+            }
+        }
+
+        private void ValidateDirection(string direction)
+        {
+            if (!char.IsLetter(direction[^1]) ||
+                !int.TryParse(direction.Substring(0, direction.Length - 1), out _))
+            {
+                Program.ErrorMsg("directions incorrect");
+            }
+        }
+
+        private void CreateDirection(string direction)
+        {
+            char cardDirection = direction[^1];
+            int numDirection = int.Parse(direction.Substring(0, direction.Length - 1));
+            Point lastPoint = TrackList.Last();
+            switch (cardDirection)
+            {
+                case 'N':
+                    for (int j = 1; j <= numDirection; j++)
+                    {
+                        TrackList.Add(lastPoint with { Y = lastPoint.Y + j });
+                    }
+                    break;
+
+                case 'S':
+                    for (int j = 1; j <= numDirection; j++)
+                    {
+                        TrackList.Add(lastPoint with { Y = lastPoint.Y - j });
+                    }
+                    break;
+
+                case 'E':
+                    for (int j = 1; j <= numDirection; j++)
+                    {
+                        TrackList.Add(lastPoint with { X = lastPoint.X + j });
+                    }
+                    break;
+
+                case 'W':
+                    for (int j = 1; j <= numDirection; j++)
+                    {
+                        TrackList.Add(lastPoint with { X = lastPoint.X - j });
+                    }
+                    break;
+
+                default:
+                    Program.ErrorMsg("direction incorrect");
+                    break;
+            }
+        }
+    }
+}
