3924a9b1e9642573c0b7b638b38b0d0f04b6ab2f
Štefan Pekník <xpekni01@vutbr.cz>
2022-06-21 04:08:31 +0200

    add more tests

--- LunchPairingTests/LunchPairingTests.cs
+++ LunchPairingTests/LunchPairingTests.cs
@@ -11,6 +11,7 @@ namespace LunchPairingTests
     [TestClass]
     public class LunchPairingTests
     {
+        // expect directory with files for testing in the same directory as exe file of tests
         private const string TestFilesPath = @"TestFiles\";
 
         private string GetTestFile(string file)
@@ -49,15 +50,15 @@ namespace LunchPairingTests
 
         [TestMethod]
         [ExpectedException(typeof(InputException))]
-        public void TypoInLunchInterval_firstPart()
+        public void TypoInLunchInterval_start()
         {
-            new LunchPairing(GetTestFile("TypoInLunchInterval_firstPart.txt"));
+            new LunchPairing(GetTestFile("TypoInLunchInterval_start.txt"));
         }
         [TestMethod]
         [ExpectedException(typeof(InputException))]
-        public void TypoInLunchInterval_secondPart()
+        public void TypoInLunchInterval_end()
         {
-            new LunchPairing(GetTestFile("TypoInLunchInterval_secondPart.txt"));
+            new LunchPairing(GetTestFile("TypoInLunchInterval_end.txt"));
         }
         [TestMethod]
         [ExpectedException(typeof(InputException))]
@@ -85,6 +86,76 @@ namespace LunchPairingTests
         public void ExtraEnter()
         {
             new LunchPairing(GetTestFile("ExtraEnter.txt"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InputException))]
+        public void NegativeLunchIntervalStart()
+        {
+            new LunchPairing(GetTestFile("NegativeLunchIntervalStart.txt"));
+        }
+
+        [TestMethod]
+        public void ZeroLunchIntervalStart()
+        {
+            new LunchPairing(GetTestFile("ZeroLunchIntervalStart.txt"));
+        }
+
+        [TestMethod]
+        public void LunchIntervalEndLongerThanDriversShift()
+        {
+            new LunchPairing(GetTestFile("LunchIntervalEndLongerThanDriversShift.txt"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NoPlaceForLunchFoundException))]
+        public void LunchIntervalStartAfterDriversShiftEnd()
+        {
+            new LunchPairing(GetTestFile("LunchIntervalStartAfterDriversShiftEnd.txt"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InputException))]
+        public void EmptyFile()
+        {
+            new LunchPairing(GetTestFile("EmptyFile.txt"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InputException))]
+        public void OnlyNewLines()
+        {
+            new LunchPairing(GetTestFile("OnlyNewLines.txt"));
+        }
+
+        [TestMethod]
+        public void LunchSpotOnLunchIntervalStart()
+        {
+            var lunch = new LunchPairing(GetTestFile("LunchSpotOnLunchIntervalStart.txt"));
+
+            Assert.AreEqual(lunch.lunchSpot, new Point(1, 0));
+        }
+
+        [TestMethod]
+        public void LunchSpotOnLunchIntervalEnd()
+        {
+            var lunch = new LunchPairing(GetTestFile("LunchSpotOnLunchIntervalEnd.txt"));
+
+            Assert.AreEqual(lunch.lunchSpot, new Point(1, 0));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InputException))]
+        public void AcceptCapitalLettersOnlyAsCardDirections()
+        {
+            new LunchPairing(GetTestFile("ExpectOnlyCapitalLettersAsCardDirections.txt"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InputException))]
+        public void AcceptPositiveNumbersOnlyAsNumericalDirections()
+        {
+            new LunchPairing(GetTestFile("AcceptPositiveNumbersOnlyAsNumericalDirections.txt"));
         }
     }
 }
